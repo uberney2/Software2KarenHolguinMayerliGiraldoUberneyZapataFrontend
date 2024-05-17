bf8c6a45d18dcac5acc747500615a11e9077d6df
Uberney Zapata <uzapatz@contratista.epm.co>
2024-05-17 17:39:26 -0500

    separate logic from productGrid

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,18 @@
 import { ProductProvider } from '../../context/ProductContext';
 import { NavbarComponent } from '../../components/navbar/NavbarComponent';
 import { ProductGrid } from '../../components/productGrid/ProductGrid';
+import { useFetchProducts } from '../../hooks/useFetchProducts';
 import './home.css';
 
 export const Home = () => {
+  const { products, isLoading } = useFetchProducts();
+  console.log(products)
   return (
     <ProductProvider>
       <NavbarComponent>
         <div className="container">
           <h3 className="product-grid-title">New Products</h3>
-          <ProductGrid />
+          <ProductGrid products = {products} isLoading = {isLoading} />
         </div>
       </NavbarComponent>
     </ProductProvider>
